3d0423a2cae18a261dd0c91a43e933fa419e8f76
goingblindbro <NOHRENTERPRISE@GMAIL.COM>
2022-04-01 16:56:35 -0700

    created nav bar, log in, sign up, nav bar logo, and logo. As well as, changed the title and subtitle to white text for better appeal.

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ const User = require("./User");
 const Wallet = require("./Wallet");
 const Coin = require("./Coin")
 
-User.hasMany(Wallet);
+User.hasone(Wallet);
 
 Wallet.belongsTo(User);
 
 Wallet.hasMany(Coin);
+
+Coin.hasMany(Wallet);
 
 module.exports = { User, Wallet };
