e254188fdbd0183dbd607c48655d56da2706c0a0
devin <devinreina99@gmail.com>
2022-04-02 14:59:51 -0700

    added wallet routes

--- controllers/api/wallet-routes.js
+++ controllers/api/wallet-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Wallet, User } = require('../../models');
 
+// route: /api/wallet
 router.get('/', (req, res) => {
     Wallet.findAll({
         attributes: ['id', 'name'],
@@ -17,5 +18,44 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// route: /api/wallet/:id
+router.get('/:id', (req, res) => {
+    Wallet.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'name'
+        ]
+    })
+    .then(dbWalletData => {
+        if (!dbWalletData) {
+          res.status(404).json({ message: 'No wallet found with this id' });
+          return;
+        }
+        res.json(dbWalletData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// route: /api/wallet/
+router.post('/', (req, res) => {
+    // expects {name: 'My Wallet', user_id: 1}
+    Wallet.create({
+      name: req.body.name,
+      user_id: req.body.user_id
+    })
+    .then(dbWalletData => res.json(dbWalletData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
 
 module.exports = router;
