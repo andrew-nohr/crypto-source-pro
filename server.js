04b5f54cfde4a3992248123bb228c707b9869a49
CPalileo <cpalileo@gmail.com>
2022-04-04 11:34:08 -0700

    commit prior to pull

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require("path");
 const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
-
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create();
+
+// const hbs = exphbs.create();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,13 +13,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.send("index");
+});
+
 app.use(routes);
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 sequelize.sync({ force: false }).then(() => {
-   app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log("Now listening"));
 });
 
 //app.listen(PORT, () => console.log("Now listening"));
