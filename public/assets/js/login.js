decb5e6298fd0433e2f86338c87646faf1e15d91
devin <devinreina99@gmail.com>
2022-04-05 20:18:58 -0700

    added signup functionality

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -25,19 +25,17 @@ async function loginFormHandler(event) {
     }
   }
   
-  /*async function signupFormHandler(event) {
+  async function signupFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
+    const username = document.querySelector('#register-id').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
+    if (username && password) {
+      const response = await fetch('/api/user', {
         method: 'post',
         body: JSON.stringify({
           username,
-          email,
           password
         }),
         headers: { 'Content-Type': 'application/json' }
@@ -45,12 +43,13 @@ async function loginFormHandler(event) {
   
       if (response.ok) {
         document.location.replace('/');
+        console.log('signed up')
       } else {
         alert(response.statusText);
       }
     }
-  }*/
+  }
   
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
   
-  /*document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);*/
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
