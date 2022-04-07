e444de67f97cfb56ffda57b7bf204d1eca01ac3a
Jason Greenia <jason.greenia@gmail.com>
2022-04-06 23:07:45 -0700

    update connection

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,13 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+let sequelize;
+
 // create connection to our db
 if (process.env.JAWSDB_URL) {
-  const sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
